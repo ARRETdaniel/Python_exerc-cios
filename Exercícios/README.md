6d1f0e472138a31034c51dd114b93eaf35da2a5f
Daniel Terra Gomes <danielterra24@hotmail.com>
2022-01-05 20:48:24 -0300

    done

--- Exercícios/README.md
+++ Exercícios/README.md
@@ -75,7 +75,7 @@
   - 3x ou mais no cartão: 20% de juros
 * Exercício Python 045: Crie um programa que faça o computador jogar Jokenpô com você.
 * Exercício Python 046: Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
-*
+* Exercício Python 047: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 
 
 # Mundo 3
--- Exercícios/ex047-numeros-pares.py
+++ Exercícios/ex047-numeros-pares.py
@@ -1,5 +1,14 @@
+# Exercício Python 047: Crie um programa que mostre na tela
+# todos os números pares que estão no intervalo entre 1 e 50.
+
 print('\033[1;30mNúmeros pares entre 1 e 50:\033[30m')
-for c in range(1, 51): # ou range(2, 51, 2), que é mais otmizado
-    if c % 2 == 0:
-        print(c, end=' ')
+for count in range(1, 51):  # range(2, 51, 2): range(2, 51, 2), que é mais otmizado
+
+    if count % 2 == 0:
+        print(count, end=' ')
+print('Fim')
+
+
+for count in range(2, 51, 2):  # range(2, 51, 2): range(2, 51, 2), que é mais otmizado
+    print(count, end=' ')
 print('Fim')
