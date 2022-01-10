b6f7e5b2e65f621a51b9c3238d3224070f5767b7
Daniel Terra Gomes <danielterra24@hotmail.com>
2022-01-10 20:04:09 -0300

    done

--- Exercícios/ex056-analizando-idades.py
+++ Exercícios/ex056-analizando-idades.py
@@ -1,10 +1,12 @@
 # Exercício Python 056: Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas.
 # No final do programa, mostre: a média de idade
 # do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+
 idadeTotal = 0
 nomeVelho = ''
 idadeVelho = 0
 novinhas = 0
+
 for c in range(1, 5):
     print('\n\033[1m{}ª Pessoa \033[m'.format(c))
 
@@ -25,6 +27,7 @@ for c in range(1, 5):
         print('\033[31mSexo Inválido\033[m')
 
 media = idadeTotal / 4
+
 print('\nMédia de Idade: {:.2f} anos'.format(media))
 
 if nomeVelho != '' and idadeVelho != 0:
